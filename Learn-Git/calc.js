cea7fa7675878b40bf30147e7536b31a831fef5d
Khoirul Husein <irul.husein31@gmail.com>
2023-05-18 14:11:43 +0700

    menambahkan belajar git

---
+++ Learn-Git/calc.js
@@ -0,0 +1,14 @@
+const add = (a, b) => a + b;
+console.log(add(1, 4));
+
+const subtract = (a, b) => a - b;
+console.log(subtract(19, 2));
+
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(10, 3));
+
+const divide = (a, b) => a / b;
+console.log(divide(11,2));
+
+const mathSin = (number) => Math.sin(number);
+console.log(mathSin(40));
