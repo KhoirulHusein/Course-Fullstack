45b6a4eb8c0eca35ce22dd1ba8b6353906e3ad70
Khoirul Husein <irul.husein31@gmail.com>
2023-05-27 16:22:24 +0700

    add: if statment

---
+++ Learn-JS/learnJS.js
@@ -0,0 +1,39 @@
+// let LemparDadu = Math.floor(Math.random() * 6) + 1;
+
+// console.log(LemparDadu);
+
+// function lemparDadu() {
+
+//  console.log(Math.floor(Math.random() * 6) + 1);
+
+// };
+
+// lemparDadu();
+
+function selamatDatang(name) {
+
+  console.log(`Hallo, ${name} Selamat Datang`);
+  console.log('');
+
+}
+
+selamatDatang('Nani');
+
+// looping js node.js
+
+const tech = 'node.js';
+
+for (let i = 0; i < 3; i++){
+
+  console.log(`Hello, world this is ${tech}`);
+
+}
+
+const belokKanan = true;
+
+if (belokKanan == false){
+  console.log('belok kekanan');
+} else {
+  console.log('belok kekiri');
+}
+
