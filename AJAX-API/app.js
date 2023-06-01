289f394ca62a878e2fab085f82a1d7493b9cf7dd
Khoirul Husein <irul.husein31@gmail.com>
2023-06-01 17:29:22 +0700

    perf: membuat lebih mudah ditulis

--- AJAX-API/app.js
+++ AJAX-API/app.js
@@ -1,16 +1,49 @@
-const req = new XMLHttpRequest();
+// const req = new XMLHttpRequest();
 
-let data;
+// let data;
 
-req.onload = function() {
-  data = JSON.parse(this.responseText);
-  console.log(data);
-};
+// // callback function berhasil
+// req.onload = function() {
+//   data = JSON.parse(this.responseText);
+//   console.log(data);
+// };
 
-req.onerror = function() {
-  console.log("error", this);
-};
+// // callback function gagal
+// req.onerror = function() {
+//   console.log("error", this);
+// };
 
-req.open('GET', 'https://swapi.dev/api/people/1');
+// req.open('GET', 'https://swapi.dev/api/people/1');
+// myReq.setRequestHeader('Accept', 'application/json');
+// req.send();
 
-req.send();
+
+//menggunakan method fetch
+fetch('https://swapi.dev/api/people/1')
+  .then((res) => {
+    if(!res.ok) {
+      throw ERROR('Could not fetch the data for that resources');
+    }
+    return res.json();
+  })
+
+  .then((data) => {
+    console.log('json req 1', data);
+    return fetch('https://swapi.dev/api/people/3')
+  })
+  
+  .then((res) => {
+    console.log('json req 2');
+    if(!res.ok) {
+      throw ERROR('Could not fetch the data for that resources');
+    }
+    return res.json();
+  })
+
+  .then((data) => {
+    console.log(data);
+  })
+
+  .catch((err) => {
+    console.log('error', err);
+  }); 
