63a4868cdd69a0a66710cce61ce7fd14709454b6
Khoirul Husein <irul.husein31@gmail.com>
2023-06-01 17:33:54 +0700

    perf: menyederhanakan code

--- AJAX-API/app.js
+++ AJAX-API/app.js
@@ -19,31 +19,50 @@
 
 
 //menggunakan method fetch
-fetch('https://swapi.dev/api/people/1')
-  .then((res) => {
-    if(!res.ok) {
-      throw ERROR('Could not fetch the data for that resources');
-    }
-    return res.json();
-  })
+// fetch('https://swapi.dev/api/people/1')
+//   .then((res) => {
+//     if(!res.ok) {
+//       throw ERROR('Could not fetch the data for that resources');
+//     }
+//     return res.json();
+//   })
 
-  .then((data) => {
-    console.log('json req 1', data);
-    return fetch('https://swapi.dev/api/people/3')
-  })
+//   .then((data) => {
+//     console.log('json req 1', data);
+//     return fetch('https://swapi.dev/api/people/3')
+//   })
   
-  .then((res) => {
-    console.log('json req 2');
-    if(!res.ok) {
-      throw ERROR('Could not fetch the data for that resources');
-    }
-    return res.json();
-  })
+//   .then((res) => {
+//     console.log('json req 2');
+//     if(!res.ok) {
+//       throw ERROR('Could not fetch the data for that resources');
+//     }
+//     return res.json();
+//   })
 
-  .then((data) => {
+//   .then((data) => {
+//     console.log(data);
+//   })
+
+//   .catch((err) => {
+//     console.log('error', err);
+//   }); 
+
+// membuat lebih sedehana dengan fetch
+const loadPeople = async () => {
+  try {
+    const req = await fetch('https://swapi.dev/api/people/1');
+    const data = await req.json();
     console.log(data);
-  })
+  
+    const req2 = await fetch('https://swapi.dev/api/people/2');
+    const data2 = await req2.json();
+    console.log(data2);
 
-  .catch((err) => {
+  } catch(err){
     console.log('error', err);
-  }); 
+  }
+
+};
+
+loadPeople();
